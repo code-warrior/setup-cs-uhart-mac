ed969843bb6560ea2baa951465992fae0e75120a
Roy A Vanegas <roy@thecodeeducators.com>
2020-03-13 14:42:56 -0400

    Add if statement to check for variables.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,12 @@
 # Although “xterm-256color” is the default for the type of TERMinal macOS is using,
 # explicitly setting it here ensures a correct value.
 export TERM=xterm-256color
+
+# If the variables.sh file exists, source it.
+if [[ -e ./includes/globals/variables.sh ]]; then
+   source ./includes/globals/variables.sh
+else
+   printf "The file variables.sh does not exist. This script requires it. Exiting...\n"
+
+   exit 1
+fi
