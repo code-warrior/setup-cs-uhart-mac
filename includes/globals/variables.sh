ffe66f158e7be64cdb2a77625662a97cbe2fdf92
Roy A Vanegas <roy@thecodeeducators.com>
2020-03-14 15:37:43 -0400

    Create a variable that contains the name of the computer

--- includes/globals/variables.sh
+++ includes/globals/variables.sh
@@ -2,3 +2,7 @@
 
 MINIMUM_MAC_OS="10.11.0"
 OS_NAME="Mac OS "
+
+# Retrieve from the system configuration utility the computer name, found in System
+# Preferences | Sharing | Computer Name.
+COMP_NAME="$(scutil --get ComputerName)"
