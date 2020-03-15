f8a0e34a5723c16293c768e2c8be363948726c6f
Roy A Vanegas <roy@thecodeeducators.com>
2020-03-15 06:00:04 -0400

    Create a variable for the local host name

--- includes/globals/variables.sh
+++ includes/globals/variables.sh
@@ -6,3 +6,7 @@ OS_NAME="Mac OS "
 # Retrieve from the system configuration utility the computer name, found in System
 # Preferences | Sharing | Computer Name.
 COMP_NAME="$(scutil --get ComputerName)"
+
+# Retrieve from the system configuration utility the local network host name, found
+# in System Preferences | Sharing | Computer Name.
+LOCAL_HOST_NAME="$(scutil --get LocalHostName)"
