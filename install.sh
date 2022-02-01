3161ccbcf85cb75299f761af705ee18c61a8bffd
Roy Vanegas <roy@thecodeeducators.com>
2022-02-01 08:34:41 -0500

    Remove how macOS version number is sniffed

--- install.sh
+++ install.sh
@@ -22,20 +22,6 @@ else
    exit 1
 fi
 
-if [[ "$MINOR_OS_NUMBER" -lt 11 ]]; then
-   print_msg "error" "This script does not support the version of Mac OS that you’re running. Please update "
-   print_msg "error" "your OS and try again."
-
-   exit 1
-else
-   if [[ "$MINOR_OS_NUMBER" -eq 11 ]]; then
-      print_msg "warn" "Your OS is outdated. Consider upgrading before continuing."
-   else
-      if [[ "$MINOR_OS_NUMBER" -gt 10 ]] && [[ "$MINOR_OS_NUMBER" -lt 16 ]]; then
-         OS_NAME="macOS"
-      fi
-   fi
-fi
 
 sudo -p "Enter your password, which, for security purposes, won’t be repeated in The Terminal as you type it: " echo "${BG_GREEN}${BLACK} > Thank you! ${RESET}"
 
