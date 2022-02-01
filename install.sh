8b1d4fad14762caa195a6bae2548d60f4ca9f536
Roy Vanegas <roy@thecodeeducators.com>
2022-02-01 18:01:10 -0500

    Install Arduino

--- install.sh
+++ install.sh
@@ -249,6 +249,21 @@ install 'Sublime Text (3.2.2)' 'Sublime Text Build 3211.dmg' 'https://download.s
   -H 'Referer: https://www.sublimetext.com/' \
   -H 'Accept-Language: en-US,en;q=0.9'
 
+install 'Arduino IDE' 'arduino-1.8.19-macosx.zip' 'https://downloads.arduino.cc/arduino-1.8.19-macosx.zip' \
+  -H 'authority: downloads.arduino.cc' \
+  -H 'sec-ch-ua: " Not;A Brand";v="99", "Google Chrome";v="97", "Chromium";v="97"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "macOS"' \
+  -H 'upgrade-insecure-requests: 1' \
+  -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36' \
+  -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
+  -H 'sec-fetch-site: same-site' \
+  -H 'sec-fetch-mode: navigate' \
+  -H 'sec-fetch-user: ?1' \
+  -H 'sec-fetch-dest: document' \
+  -H 'referer: https://www.arduino.cc/' \
+  -H 'accept-language: en-US,en;q=0.9'
+
 install 'Zoom' 'Zoom.pkg' 'https://cdn.zoom.us/prod/5.9.3.4239/Zoom.pkg'
 
 #####################################################################################
