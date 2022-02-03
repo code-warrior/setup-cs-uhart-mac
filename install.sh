6389ad0ba8d6cae82e73429e1fd863a13a45eb45
Roy Vanegas <roy@thecodeeducators.com>
2022-02-03 10:23:20 -0500

    Install Visual Studio Code

--- install.sh
+++ install.sh
@@ -292,6 +292,22 @@ install 'Java SE Development Kit' 'jdk-17_macos-x64_bin.dmg' 'https://download.o
   -H 'Accept-Language: en-US,en;q=0.9'
 
 print_msg "log" "Installing IDEs and editors..."
+
+install 'Visual Studio Code' 'VSCode-darwin-universal.zip' 'https://az764295.vo.msecnd.net/stable/899d46d82c4c95423fb7e10e68eba52050e30ba3/VSCode-darwin-universal.zip' \
+  -H 'authority: az764295.vo.msecnd.net' \
+  -H 'upgrade-insecure-requests: 1' \
+  -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36' \
+  -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
+  -H 'sec-fetch-site: cross-site' \
+  -H 'sec-fetch-mode: navigate' \
+  -H 'sec-fetch-user: ?1' \
+  -H 'sec-fetch-dest: document' \
+  -H 'sec-ch-ua: " Not;A Brand";v="99", "Google Chrome";v="97", "Chromium";v="97"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "macOS"' \
+  -H 'referer: https://code.visualstudio.com/' \
+  -H 'accept-language: en-US,en;q=0.9'
+
 install 'Sublime Text (3.2.2)' 'Sublime Text Build 3211.dmg' 'https://download.sublimetext.com/Sublime%20Text%20Build%203211.dmg' \
   -H 'Connection: keep-alive' \
   -H 'sec-ch-ua: " Not;A Brand";v="99", "Google Chrome";v="97", "Chromium";v="97"' \
