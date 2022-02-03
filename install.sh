4aeeb7bab4d147ba93671a3aa1f7108a03c6ac70
Roy Vanegas <roy@thecodeeducators.com>
2022-02-03 10:25:54 -0500

    Add a TODO comment

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# TODO
+# * Install VS Code, then Spotless
+# * Install Node
+
 # Although “xterm-256color” is the default for the type of TERMinal macOS is using,
 # explicitly setting it here ensures a correct value.
 export TERM=xterm-256color
