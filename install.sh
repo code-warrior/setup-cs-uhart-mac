1d1a6cd9e12214bd4fbdd5f0dea64e69f07995b4
Roy Vanegas <roy@thecodeeducators.com>
2022-02-04 07:10:06 -0500

    Install Atom

--- install.sh
+++ install.sh
@@ -360,6 +360,21 @@ install 'Sublime Text (3.2.2)' 'Sublime Text Build 3211.dmg' 'https://download.s
   -H 'Referer: https://www.sublimetext.com/' \
   -H 'Accept-Language: en-US,en;q=0.9'
 
+install 'Atom' 'atom-mac.zip' 'https://atom-installer.github.com/v1.58.0/atom-mac.zip?s=1627025609&ext=.zip' \
+  -H 'authority: atom-installer.github.com' \
+  -H 'upgrade-insecure-requests: 1' \
+  -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36' \
+  -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
+  -H 'sec-fetch-site: cross-site' \
+  -H 'sec-fetch-mode: navigate' \
+  -H 'sec-fetch-user: ?1' \
+  -H 'sec-fetch-dest: document' \
+  -H 'sec-ch-ua: " Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "macOS"' \
+  -H 'referer: https://atom.io/' \
+  -H 'accept-language: en-US,en;q=0.9'
+
 install 'Arduino IDE' 'arduino-1.8.19-macosx.zip' 'https://downloads.arduino.cc/arduino-1.8.19-macosx.zip' \
   -H 'authority: downloads.arduino.cc' \
   -H 'sec-ch-ua: " Not;A Brand";v="99", "Google Chrome";v="97", "Chromium";v="97"' \
